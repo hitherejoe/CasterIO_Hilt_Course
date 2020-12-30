8f41c6209c13df4297014fa8725a83a30475ef9b
hitherejoe <hitherejoebirch@gmail.com>
2020-12-30 08:59:42 +0000

    Create water tracking activity

---
+++ app/src/main/java/co/joebirch/watertracker/WaterTrackerActivity.kt
@@ -0,0 +1,12 @@
+package co.joebirch.watertracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaterTrackerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water_tracker)
+    }
+}
