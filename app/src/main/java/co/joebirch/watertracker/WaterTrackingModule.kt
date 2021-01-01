58ee260c10c77fc20036ed90c74f0267b71309bd
hitherejoe <hitherejoebirch@gmail.com>
2021-01-01 09:11:13 +0000

    Setup viewmodel

--- app/src/main/java/co/joebirch/watertracker/WaterTrackingModule.kt
+++ app/src/main/java/co/joebirch/watertracker/WaterTrackingModule.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.FragmentComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 
 @InstallIn(FragmentComponent::class)
