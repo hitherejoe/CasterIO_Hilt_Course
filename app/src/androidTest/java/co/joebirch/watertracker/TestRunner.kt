9579164b0efc1793c7c4a8023a1f3d740a44f6ef
hitherejoe <hitherejoebirch@gmail.com>
2021-01-01 09:35:21 +0000

    Rename runner

--- app/src/androidTest/java/co/joebirch/watertracker/TestRunner.kt
+++ app/src/androidTest/java/co/joebirch/watertracker/TestRunner.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
 
-class TestApplication: AndroidJUnitRunner() {
+class TestRunner: AndroidJUnitRunner() {
 
     override fun newApplication(
         cl: ClassLoader?,
