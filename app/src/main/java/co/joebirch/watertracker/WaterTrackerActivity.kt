b41c7792b5d68e0e88dc6cc096bfe5ab1756cfd8
hitherejoe <hitherejoebirch@gmail.com>
2021-01-01 08:53:19 +0000

    Setup injection for water tracking activity

--- app/src/main/java/co/joebirch/watertracker/WaterTrackerActivity.kt
+++ app/src/main/java/co/joebirch/watertracker/WaterTrackerActivity.kt
@@ -2,13 +2,31 @@ package co.joebirch.watertracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_water_tracker.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class WaterTrackerActivity : AppCompatActivity(), WaterIntakePreferenceListener {
+
+    @Inject lateinit var preferencesHelper: PreferencesHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_water_tracker)
+
+        waterCountText.text = preferencesHelper.getWaterIntake().toString()
+
+        trackWaterButton.setOnClickListener {
+            preferencesHelper.incrementWaterIntake()
+        }
+
+        preferencesHelper.subscribeToWaterIntakeChanges(this)
+    }
+
+    override fun onDestroy() {
+        preferencesHelper.unsubscribeFromWaterIntakeChanges()
+        super.onDestroy()
     }
 
     override fun onWaterIntakeChanged(waterIntake: Int) {
