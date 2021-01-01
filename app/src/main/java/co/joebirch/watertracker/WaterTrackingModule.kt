4ba64d0e8cd9ab900526776829af586c43c6b741
hitherejoe <hitherejoebirch@gmail.com>
2021-01-01 09:11:51 +0000

    Use FragmentComponent

--- app/src/main/java/co/joebirch/watertracker/WaterTrackingModule.kt
+++ app/src/main/java/co/joebirch/watertracker/WaterTrackingModule.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.FragmentComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 
-@InstallIn(ActivityComponent::class)
+@InstallIn(FragmentComponent::class)
 @Module
 class WaterTrackingModule {
 
